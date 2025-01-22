a7b5496c38fa5ee66442eb12a576c7559d24bb2d
josh <96921768+joshwakelin@users.noreply.github.com>
2025-01-22 12:44:34 -0500

    fix lookup()

--- display/message.py
+++ display/message.py
@@ -1,7 +1,8 @@
 code = {
     1: "[ERROR] No reader has been detected. Retrying in 5 seconds..",
     2: "[ERROR] Failed to find reader. Please retry when a reader is connected.",
-    3: "[ERROR] Failed to detect card type."
+    3: "[ERROR] Failed to detect card type.",
+    4: "[ERROR] Failed to detect card type."
 }
 
 
